7b2507000ce069b833d6c81b1af5b23a40668a1c
alexanderishoy <alexanderishoy@gmail.com>
2026-09-13 11:30:12 -0600

    General info added to the end of main.py

--- main.py
+++ main.py
@@ -11,3 +11,42 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+"""
+Official Statements from Toby Fox:
+With Chapter 5, we completed the last of the necessary setup for Chapter 6 and 7.
+From Chapter 6 onward, there are no more wacky silly adventures, 
+and you will begin to see the results of the setup that has been made so far.
+Chapter 6 is a shorter, straightforward chapter that focuses on one aspect of the story.
+Because of that, many things are still not touched on here – in fact, it may surprise you what was missing!
+But one thing will be clarified, whether you’re ready or not.
+Some of you may say “Wait! You need to make the chapters longer, not shorter!” 
+That’s an extremely reasonable concern, but... 
+by the end of Chapter 7 I’m 100% sure you’ll understand why I feel confident about the way I’m telling this story. 
+Don’t worry about the pacing until it’s over!
+
+Barring any major restructuring, the main things that will continue to need development time are
+the boss of the chapter and polishing cutscenes.
+
+However, I do think it’s important to mention, sometimes you don’t have all the information, 
+or I had a different expectation about how certain info would be interpreted or combined.
+
+By the way, I told Fangamer not to expect to make too much merchandise for Chapter 6 and 7.
+
+Anyway, there aren’t any more Chapters where we focus on goofy, 
+attention-hungry Darkner characters with the potential to win weird popularity contests.
+"""
+"""
+Observations from existing gameplay/general forshadowing:
+Every secret boss has in someway forshadowed the next main boss of the following chapter.
+Jevil mentions Queen, Spamton mentions Mike and CRTs, Gerson mentions the field of Pink and Gold being consumed in an inferno of jealosy (Pink)...
+And the point of Pink's fight is wrestling over control of your body before eventually becoming whole.
+Now who do we know that struggles to control their actions?
+Kris.
+Therefore, Kris will either be the next main boss of the game, or the central source of the conflict. I found quite the compelling theory video...
+https://www.youtube.com/watch?v=7TEN6PSlFxg&t=1691s
+Oh great he's been swayed by another theory video!
+
+But if it's true, then all the previous thoughts about chapter 6 will be like...
+https://www.youtube.com/watch?v=-apX5FcEDZY
+"""
